8b8ff68ed7d9346cce6fa74745f90f3383f234f8
kaungart <kaungthantkyaw.it@gmail.com>
2026-08-10 16:05:19 +0630

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,12 @@
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.Scanner;
+import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) throws Exception
@@ -12,8 +19,8 @@ public class Main {
             System.out.print("$ ");
             String input = scanner.nextLine();
 
-            String cmd = input.indexOf(" ") == -1 ? input : input.substring(0, input.indexOf(" "));
-            String rem = input.indexOf(" ") == -1 ? "" : input.substring(input.indexOf(" ") + 1);
+            String cmd = !input.contains(" ") ? input : input.substring(0, input.indexOf(" "));
+            String rem = !input.contains(" ") ? "" : input.substring(input.indexOf(" ") + 1);
 
             if (cmd.equals("exit")) {
                 break;
@@ -23,8 +30,32 @@ public class Main {
             }
             else if (cmd.equals("type")) {
                 boolean isBuiltInCmd = builtInCommands.contains(rem);
-                if (isBuiltInCmd) System.out.println(rem + " is a shell builtin");
-                else System.out.println(rem + ": not found");
+                if (isBuiltInCmd) {
+                    System.out.println(rem + " is a shell builtin");
+                }
+                else {
+//                    TODO: search executable in PATH variable
+                    String pathVariable = System.getenv("PATH");
+                    String[] directories = pathVariable.split(File.pathSeparator);
+                    boolean isWindows = System.getProperty("os.name").toLowerCase().contains("win");
+
+//                    TODO: search executables that match to rem in all dir
+                    Predicate<String> pred = dir -> {
+                        Path exePath = isWindows ? Paths.get(dir).resolve(rem + ".exe") : Paths.get(dir).resolve(rem);
+
+                        return Files.isRegularFile(exePath) && Files.isExecutable(exePath) && Files.isExecutable(exePath);
+                    };
+
+                    Arrays.stream(directories).filter(pred).findFirst()
+                            .ifPresentOrElse(
+                                    dir -> {
+                                        System.out.println(rem + " is " + Paths.get(dir).resolve(rem));
+                                    },
+                                    () -> {
+                                        System.out.println(rem + ": not found");
+                                    }
+                            );
+                }
             }
             else {
                 System.out.println(input + ": command not found");
